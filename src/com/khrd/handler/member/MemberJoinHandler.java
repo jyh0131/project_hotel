66857c8bcf6dca526827ee0a955164a94a21895d
you-iu <kdjgma1@naver.com>
2019-10-23 14:08:38 +0900

    member(4)

--- src/com/khrd/handler/member/MemberJoinHandler.java
+++ src/com/khrd/handler/member/MemberJoinHandler.java
@@ -18,7 +18,7 @@ public class MemberJoinHandler implements CommandHandler {
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		if(request.getMethod().equalsIgnoreCase("get")) {
-			return "/WEB-INF/view/joinForm.jsp";
+			return "/WEB-INF/view/member/joinForm.jsp";
 		}else if(request.getMethod().equalsIgnoreCase("post")) {
 			String name = request.getParameter("name");
 			String date = request.getParameter("date");
